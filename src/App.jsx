02273d46a3f9a9e1859f4605eedd09dcb411f3ff
nathan8b <nb803622@ohio.edu>
2025-08-26 20:18:44 -0400

    Added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import PersonalInfo from './components/PersonalInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({ name: "", email: "", phone: "", location: ""});
   const [education, setEducation] = useState([{school: "", degree: "", startDate: "", endDate: "", gpa: ""}]);
   const [experience, setExperience] = useState([{company: "", position: "", startDate: "", EndDate: "", description: ""}]);
+  const [projects, setProjects] = useState([{title: "", tools: "", startDate: "", endDate: "", link: "", description: ""}]);
 
   return (
     <>
@@ -15,6 +17,7 @@ function App() {
         <PersonalInfo data={personalInfo} setData={setPersonalInfo} />
         <Education data={education} setData={setEducation} />
         <Experience data={experience} setData={setExperience} />
+        <Projects data={projects} setData={setProjects}/>
       </div>
 
       <div className="preview-container">
---
+++ src/components/Projects.jsx
@@ -0,0 +1,77 @@
+function Projects({data, setData}) {
+    const handleChange = (index, field, value) => {
+        const newProject = [...data];
+        newProject[index][field] = value;
+        setData(newProject);
+    }
+
+    const addProject = () => {
+        setData([...data, {company: "", position: "", startDate: "", EndDate: "", description: ""}]);
+    }
+
+    const removeProject = (index) => {
+        const newProject = data.filter((_, i) => i !== index);
+        setData(newProject);
+    }
+
+    return (
+        <div className="section">
+            <h2>Projects</h2>
+            {data.map((entry, index) => (
+                <div key={index} className="sub-section">
+
+                    <div className="entry-header">
+                        <h3 className="sub-title">Project #{index+1}</h3>
+                        <button className="delete-button" onClick={() => removeProject(index)}>Delete</button>
+                    </div>
+                    
+                    <label for="title">Project Title:</label>
+                    <input 
+                        type="text"
+                        placeholder="Enter project title"
+                        value={entry.title}
+                        onChange={(event) => handleChange(index, "title", event.target.value)}
+                    />
+                    <label for="tools">Tools:</label>
+                    <input 
+                        type="text"
+                        placeholder="Ex: JavaScript, React, CSS"
+                        value={entry.tools}
+                        onChange={(event) => handleChange(index, "tools", event.target.value)}
+                    />
+                    <label for="start-date">Start Date:</label>
+                    <input 
+                        type="date"
+                        placeholder="Start date"
+                        value={entry.startDate}
+                        onChange={(event) => handleChange(index, "startDate", event.target.value)}
+                    />
+                    <label for="end-date">End Date:</label>
+                    <input 
+                        type="date"
+                        placeholder="End date"
+                        value={entry.endDate}
+                        onChange={(event) => handleChange(index, "endDate", event.target.value)}
+                    />
+                    <label for="description">Description:</label>
+                    <textarea
+                        rows={6}
+                        placeholder="Enter a description"
+                        value={entry.description}
+                        onChange={(event) => handleChange(index, "description", event.target.value)}
+                    />
+                    <label for="link">Link (optional):</label>
+                    <input 
+                        type="link"
+                        placeholder="Repository or project link"
+                        value={entry.link}
+                        onChange={(event) => handleChange(index, "link", event.target.value)}
+                    />
+                </div>
+            ))}
+            <button onClick={addProject}>Add</button>
+        </div>
+    );
+}
+
+export default Projects;
