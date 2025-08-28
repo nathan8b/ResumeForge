8ca3777e0ad8dd6abc70d170e993833e9061b9f9
nathan8b <nb803622@ohio.edu>
2025-08-27 23:37:11 -0400

    added skills section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import PersonalInfo from './components/PersonalInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({ name: "", email: "", phone: "", linkedin: "", github: ""});
   const [education, setEducation] = useState([{school: "", location: "", degree: "", startDate: "", endDate: "", gpa: ""}]);
   const [experience, setExperience] = useState([{company: "", location: "", position: "", startDate: "", EndDate: "", description: ""}]);
   const [projects, setProjects] = useState([{title: "", tools: "", startDate: "", endDate: "", link: "", description: ""}]);
+  const [skills, setSkills] = useState({languages: "", frameworks: "", tools: ""});
 
   return (
     <>
@@ -18,6 +20,7 @@ function App() {
         <Education data={education} setData={setEducation} />
         <Experience data={experience} setData={setExperience} />
         <Projects data={projects} setData={setProjects}/>
+        <Skills data={skills} setData={setSkills}/>
       </div>
 
       <div className="preview-container">
---
+++ src/components/Skills.jsx
@@ -0,0 +1,33 @@
+function Skills({data, setData}) {
+    return (
+        <div className="section">
+            <h2>Skills</h2>
+
+            <label for="languages">Languages:</label>
+            <input 
+                type="text"
+                placeholder="Ex: JavaScript, Python, C++..."
+                value={data.languages}
+                onChange={(event) => setData({ ...data, languages: event.target.value})}
+            />
+
+            <label for="frameworks">Frameworks:</label>
+                <input 
+                    type="text"
+                    placeholder="Ex: React, Node.js, Flask..."
+                    value={data.frameworks}
+                    onChange={(event) => setData({ ...data, frameworks: event.target.value})}
+            />
+
+            <label for="tools">Tools:</label>
+            <input 
+                type="text"
+                placeholder="Ex: Git, VS Code, Docker..."
+                value={data.tools}
+                onChange={(event) => setData({ ...data, tools: event.target.value})}
+            />
+        </div>
+    )
+}
+
+export default Skills;
